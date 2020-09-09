dd30bf3efc8cc4813f0fe4d9478629d188fa9742
takanoharuka <haruka.takano0111@gmail.com>
2020-09-09 13:44:58 +0900

    comment_function_exit

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to:'tweets#index'
   resources :tweets do
-    collection do
-      get 'search'
-    end
+    resources :comments, only: [:create, :destroy]
+      collection do
+        get 'search'
+      end
   end
 end
